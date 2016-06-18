f1b9ea5ca10e444e94cd9f1f118be71e11604eb8
cheezgi <zackh@firemail.cc>
2016-06-17 21:01:06 -0400

    squelch push.default message

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <readline/readline.h> /* TODO: add windows/mac variants */
 #include <readline/history.h>
 
+#include "mpc.h"
+
 int main()
 {
     /* this is about to get a whole lot uglier */
